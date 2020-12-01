c7f85223b5009c4578a1e1ef770a3e78561bbf78
Xiao(Iris) Jiang <jiang.xiao@dhs.sg>
2020-12-02 01:38:35 +0800

    edit profile page added
    Allow logged in user to see and update his/her personal information

--- Frontend/src/components/pages/Profile.js
+++ Frontend/src/components/pages/Profile.js
@@ -1,37 +1,36 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import HeroSection from '../HeroSection';
-import Link from '@material-ui/core/Link';
 
-
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
+const EditInput = ({value, onChange, edit}) => {
+  return (
+    edit ? <input
+      className='value form-input'
+      value={value}
+      onChange={event => onChange(event.target.value)}
+    /> : <div className='value'>{value}</div>
+  )
+}
 
 
 export function Profile(props) {
-  const [state, setState] = React.useState({
-    user: {},
-    userinfo: {},
-    loading: true
+  const [userInfo, setUserInfo] = React.useState({
+    edit: false,
+    ID: 1,
+    SleepStart: 0,
+    SleepEnd: 8,
+    BudgetLow: 2000,
+    BudgetHigh: 3000,
+    Gender: 'man',
+    Pet: 1,
+    Parking: 1,
+    Comment: 'easygoing'
+  })
+
+  const [preference, setPreference] = React.useState({
+    edit: false,
+    SleepStart: 0,
+    SleepEnd: 8,
+    Gender: 'man',
+    Pet: 1,
   })
 
   React.useEffect(() => {
@@ -40,56 +39,92 @@ export function Profile(props) {
       const _user = await fetch("http://localhost:3000/users/1");
       const userjson = await _user.json();
       const userinfojson = await _userinfo.json();
-      setState({
-        user: userjson,
-        userinfo: userinfojson
-      })
+
     }
-  
-    fetchThings();
+
   }, []);
 
-  const classes = useStyles();
-
-  if (state.loading) {
-    return null;
-  }
   return (
-  <Card className={classes.root}>
-    <CardContent>
-      <Typography className={classes.title} color="textSecondary" gutterBottom>
-        {state.user.Email}
-      </Typography>
-      <Typography variant="h5" component="h2">
-        {state.user.Name}
-      </Typography>
-      <Typography className={classes.pos} color="textSecondary">
-        {state.userinfo.Gender}
-      </Typography>
-      <Typography variant="body1" component="p">
-        Sleep Hours: {state.userinfo.SleepStart}:00 - {state.userinfo.SleepEnd}:00 <br></br>
-        Budget: {state.userinfo.BudgetLow} - {state.userinfo.BudgetHigh} <br></br>
-        { parseInt(state.userinfo.Pet) === 0 ? (<div> Pet: None </div> ) : (<div> Pet: Yes </div>) } <br></br>
-        His Apartment Info: 123 Hilgard Rm 123, 1b1b, no parking, no gym <br></br>
-        His Roomamtes: <Link href="#" >
-Rick Wang
-</Link>
-      <br></br> 
-      <br></br>   
-      <Typography variant="subtitle1" component = "p">
-        Reviews by other Bruins: <br/>
-      </Typography>
-      </Typography>
-      <Typography variant="body2" component="p">
-        Nice person and easy to live with 
-        <br />
-        Wonderful Roomate
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small" color = "primary">Back</Button>
-    </CardActions>
-  </Card>
+    <div className="signup" style={{overflow: "auto"}}>
+      <div className="form-container" style={{height: 'auto',width:600,marginTop:250,marginBottom:44}}>
+        <div className="form-content" style={{paddingBottom: 40}}>
+          <div style={{textAlign: "center"}}>UserInfo
+            <span className='edit' onClick={() => setUserInfo({
+              ...userInfo,
+              edit: !userInfo.edit
+            })}>{userInfo.edit ? 'save' : 'edit'}</span>
+          </div>
+          <div className='display-row'>
+            <div className='key'>SleepStart:</div>
+            <EditInput value={userInfo.SleepStart} edit={userInfo.edit}
+                       onChange={v => setUserInfo({...userInfo, SleepStart: v})}/>
+          </div>
+          <div className='display-row'>
+            <div className='key'>SleepEnd:</div>
+            <EditInput value={userInfo.SleepEnd} edit={userInfo.edit}
+                       onChange={v => setUserInfo({...userInfo, SleepEnd: v})}/>
+          </div>
+          <div className='display-row'>
+            <div className='key'>BudgetLow:</div>
+            <EditInput value={userInfo.BudgetLow} edit={userInfo.edit}
+                       onChange={v => setUserInfo({...userInfo, BudgetLow: v})}/>
+          </div>
+          <div className='display-row'>
+            <div className='key'>BudgetHigh:</div>
+            <EditInput value={userInfo.BudgetHigh} edit={userInfo.edit}
+                       onChange={v => setUserInfo({...userInfo, BudgetHigh: v})}/>
+          </div>
+          <div className='display-row'>
+            <div className='key'>Gender:</div>
+            <EditInput value={userInfo.Gender} edit={userInfo.edit}
+                       onChange={v => setUserInfo({...userInfo, Gender: v})}/>
+          </div>
+          <div className='display-row'>
+            <div className='key'>Pet:</div>
+            <EditInput value={userInfo.Pet} edit={userInfo.edit}
+                       onChange={v => setUserInfo({...userInfo, Pet: v})}/>
+          </div>
+          <div className='display-row'>
+            <div className='key'>Parking:</div>
+            <EditInput value={userInfo.Parking} edit={userInfo.edit}
+                       onChange={v => setUserInfo({...userInfo, Parking: v})}/>
+          </div>
+          <div className='display-row'>
+            <div className='key'>Comment:</div>
+            <EditInput value={userInfo.Comment} edit={userInfo.edit}
+                       onChange={v => setUserInfo({...userInfo, Comment: v})}/>
+          </div>
+
+          <div style={{textAlign: "center", marginTop: 14}}>Preference
+            <span className='edit'
+                  onClick={() => setPreference({
+                    ...preference,
+                    edit: !preference.edit
+                  })}>{preference.edit ? 'save' : 'edit'}</span>
+          </div>
+          <div className='display-row'>
+            <div className='key'>SleepStart:</div>
+            <EditInput value={preference.SleepStart} edit={preference.edit}
+                       onChange={v => setPreference({...preference, SleepStart: v})}/>
+          </div>
+          <div className='display-row'>
+            <div className='key'>SleepStart:</div>
+            <EditInput value={preference.SleepEnd} edit={preference.edit}
+                       onChange={v => setPreference({...preference, SleepEnd: v})}/>
+          </div>
+          <div className='display-row'>
+            <div className='key'>Gender:</div>
+            <EditInput value={preference.Gender} edit={preference.edit}
+                       onChange={v => setPreference({...preference, Gender: v})}/>
+          </div>
+          <div className='display-row'>
+            <div className='key'>Pet:</div>
+            <EditInput value={preference.Pet} edit={preference.edit}
+                       onChange={v => setPreference({...preference, Pet: v})}/>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
