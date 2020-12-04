eacddff332d8572160d8d96429764591705642a1
Yining Wang <36571521+kyswn@users.noreply.github.com>
2020-12-03 16:23:47 -0800

    gitignore

--- Backend/api/config/db.config.js
+++ Backend/api/config/db.config.js
@@ -1,7 +1,7 @@
 //write your own config here
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "password",
-    DB: "main"
-  };
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "Nfls20110909",
+  DB: "main"
+}; 
