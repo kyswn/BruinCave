6b0896eeecd4382b47badc39e579d76639d02a0a
Xiao(Iris) Jiang <jiang.xiao@dhs.sg>
2020-12-05 03:04:41 +0800

    Update Profile.test.js

--- Frontend/src/components/pages/Profile.test.js
+++ Frontend/src/components/pages/Profile.test.js
@@ -15,3 +15,39 @@ it("renders Account header", () => {
   const welcome = <div className='key'>SleepStart:</div>;
   expect(wrapper.contains(welcome)).toEqual(true);
 });
+
+it("renders Account header", () => {
+  const wrapper = shallow(<Profile />);
+  const welcome = <div className='key'>SleepEnd:</div>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
+
+it("renders Account header", () => {
+  const wrapper = shallow(<Profile />);
+  const welcome = <div className='key'>BudgetLow:</div>
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
+
+it("renders Account header", () => {
+  const wrapper = shallow(<Profile />);
+  const welcome = <div className='key'>BudgetHigh:</div>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
+
+it("renders Account header", () => {
+  const wrapper = shallow(<Profile />);
+  const welcome = <div className='key'>Gender:</div>
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
+
+it("renders Account header", () => {
+  const wrapper = shallow(<Profile />);
+  const welcome = <div className='key'>Parking:</div>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
+
+it("renders Account header", () => {
+  const wrapper = shallow(<Profile />);
+  const welcome = <div className='key'>Pet:</div>
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
